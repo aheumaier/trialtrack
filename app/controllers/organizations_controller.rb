c0ca3f908f2e9c060182568f1e0aa79eb1417f4f
Andreas Heumaier <developer@andreasheumaier.de>
2014-11-09 21:35:57 +0100

    WIP: patches

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def organization_params
-      params.require(:organization).permit(:id, :address_id)
+      params.require(:organization).permit(:id, :name, address_params: [ :street, :city, :city_code, :country ])
     end
 end
