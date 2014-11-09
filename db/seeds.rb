bfd1a55a442b4d5a9721faa21bd8a5207fae77e9
Andreas Heumaier <developer@andreasheumaier.de>
2014-11-09 20:27:04 +0100

    make showing addres working

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,18 @@
 QuestionType.create([{name: "Freitext"}, {name: "Ja/Nein"}, {name: "Multiple choice"}])
 roles = Role.create([{name: "Superuser"}, {name: "Teilnehmer"}, {name: "Admin"}])
 
-addresses = Address.create([{}, {}, {}, {}])
-organizations = Organization.create([{name: "Medicore", address: addresses[0]}, {name: "Company1", address: addresses.last}])
+addresses = Address.create([{street:"mystreet 1", city: "Hamburg", city_code: 234234, country: "Germany"},
+ {street:"mystreet 2", city: "Hamburg", city_code: 234234, country: "Germany"},
+ {street:"mystreet 3", city: "Hamburg", city_code: 234234, country: "Germany"},
+ {street:"mystreet 4", city: "Hamburg", city_code: 234234, country: "Germany"},
+ {street:"mystreet 5", city: "Hamburg", city_code: 234234, country: "Germany"}]
+)
+organizations = Organization.create([{name: "Medicore", address: addresses[0]}, {name: "Company1", address: addresses[1]}])
 
-User.create([{first_name: "Andreas", last_name: "Heumaier", role_id: roles[0].id, address: addresses[0], organization_id: organizations[0].id},
-					  {first_name: "Christoph", last_name: "Klaja", role_id: roles[0].id, address: addresses[1], organization_id: organizations[0].id},
-					  {first_name: "Admin", last_name: "User", role_id: roles[2].id, address: addresses[2], organization_id: organizations[1].id},
-					  {first_name: "Trial", last_name: "User", role_id: roles[1].id, address: addresses[3], organization_id: nil}]
+User.create([{first_name: "Andreas", last_name: "Heumaier", role_id: roles[0].id, address: addresses[2], organization_id: organizations[0].id},
+					  {first_name: "Christoph", last_name: "Klaja", role_id: roles[1].id, address: addresses[3], organization_id: organizations[0].id},
+					  {first_name: "Admin", last_name: "User", role_id: roles[2].id, address: addresses[4], organization_id: organizations[1].id},
+					  {first_name: "Trial", last_name: "User", role_id: roles[1].id, address: addresses[4], organization_id: nil}]
 					)
 
 
