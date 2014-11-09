311352a710ad48c1967b3c278daa9e4368e3cf13
Andreas Heumaier <developer@andreasheumaier.de>
2014-11-09 19:55:27 +0100

    Fix for form loaders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @roles = Role.all
+    @organizations = Organization.all
   end
 
   # POST /users
@@ -69,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:id, :first_name, :last_name, :role_id, :address_id, :organization_id)
+      params.require(:user).permit(:id, :first_name, :last_name, :role_id, :organization_id, :email, address_params: [ :street, :city, :city_code, :country ])
     end
 end
