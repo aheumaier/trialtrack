51cbae7fcf045a08a80f8b27560b736da608e4d5
Christoph Klaja <chris@aixtraordinary.de>
2014-12-13 18:07:22 +0100

    has_many :through user <--> trial

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -1,4 +1,5 @@
 class Trial < ActiveRecord::Base
   has_many :trials_users
+  has_many :users, :through => :trials_users
   belongs_to :organization
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   belongs_to :role
   has_one :address, as: :addressable
-  belongs_to :organization
-  has_one :trials_user
-  accepts_nested_attributes_for :address, :trials_user
+  belongs_to :organization 
+  has_many :trials_users
+  has_many :trials, :through => :trials_users
+  accepts_nested_attributes_for :address, :trials
 
 
   def full_name
